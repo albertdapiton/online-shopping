71426c17a3563bd60986261c90fe6266e006e171
Alber Dapiton <albert_dapiton@ligph.com>
2021-05-20 14:18:00 +0800

    Add React Login

---
+++ be/resources/js/Router.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {BrowserRouter, Link, Route, Switch} from 'react-router-dom';
+
+import Home from './components/Home/Home';
+import Login from './views/Login/Login';
+import NotFound from './views/NotFound/NotFound';
+
+const Main = props => (
+    <Switch>
+        <Route exact path='/' component={Home}/>
+        <Route path='/login' component={Login}/>
+        <Route component={NotFound}/>
+    </Switch>
+);
+
+export default Main;
---
+++ be/resources/js/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <footer>
+            Whatever normally goes into the footer
+        </footer>
+    );
+}
+
+export default Footer
